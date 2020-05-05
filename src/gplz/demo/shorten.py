0c6aa71b7cbb8d85d8f10d2977e52c5d4e87478a
Roy Mathew <rmathew8@gmail.com>
2020-05-05 12:51:24 -0400

    added creation ts and access count

--- src/gplz/demo/shorten.py
+++ src/gplz/demo/shorten.py
@@ -1,5 +1,6 @@
 import hashlib
 import urllib.parse
+import datetime
 import base64
 from pprint import pprint
 import string
@@ -10,10 +11,10 @@ URLLEN = 10  # max chars in shortened url
 CHOICES = string.printable[:62]  # select only digits and letters
 """
 maintain a lookup table of:
-  {sha: {url, shortcode}}
+  _cache = {sha: {url, shortcode, created, access_count}}
 
 and a set of shortcodes for reverse lookup
-  {shortcode: sha}
+  _shortcodes = {shortcode: sha}
 """
 
 _cache = {}
@@ -27,20 +28,6 @@ def new_shortcode():
             return shortcode
 
 
-def lookup(shortcode):
-    try:
-      sha = _shortcodes[shortcode]
-      return _cache[sha]
-    except:
-      raise Exception(f'no sha found in {"/".join(list(_shortcodes))}')
-
-# clear global cache (for testing)
-def clear():
-    global _cache
-    global _shortcodes
-    _cache, _shortcodes = {}, {}
-
-
 def shorten(url):
     m = hashlib.sha256()
     m.update(url)
@@ -51,8 +38,27 @@ def shorten(url):
 
     # need new short-code; generate and add to cache
     shortcode = new_shortcode()
-    _cache[sha] = {'shortcode': shortcode, 'url': url}
+    ts = datetime.datetime.now().timestamp()
+    _cache[sha] = {'shortcode': shortcode, 'url': url, 'created': ts}
     _shortcodes[shortcode] = sha
 
     return 'new', shortcode
+
+
+def lookup(shortcode):
+    try:
+        sha = _shortcodes[shortcode]
+        res = _cache[sha]
+        res.setdefault('access_count', 0)
+        res['access_count'] += 1
+        return res
+    except:
+        raise Exception(f'no sha found in {"/".join(list(_shortcodes))}')
+
+
+# clear global cache (for testing)
+def clear():
+    global _cache
+    global _shortcodes
+    _cache, _shortcodes = {}, {}
 
--- src/gplz/demo/tests/test_shorten.py
+++ src/gplz/demo/tests/test_shorten.py
@@ -2,17 +2,27 @@ import pytest
 from gplz.demo import shorten
 
 def test_basic():
+  # basic functionality
   url = b"some/very/long/url?with-arg1=xx&arg2=yy"
   assert shorten.shorten(url) == ('new', 'EkCCu4MWoB')
+  # test caching
   assert shorten.shorten(url) == ('cached', 'EkCCu4MWoB')
 
+  # basic functionality with a second url; test caching
   url2 = b"some/other/url?with-arg1=abb&arg2=yy"
   assert shorten.shorten(url2) == ('new', 'tGsoaoYuAO')
   assert shorten.shorten(url2) == ('cached', 'tGsoaoYuAO')
 
+  # test lookup of short codes
   assert shorten.lookup('tGsoaoYuAO')['url'] == url2
+  # test attributes of short codes
+  assert shorten.lookup('tGsoaoYuAO')['access_count'] == 2
+  assert shorten.lookup('tGsoaoYuAO')['created']
+
+  # negative test
   with pytest.raises(Exception, match='no sha'):
     assert shorten.lookup('junk')
 
+  # clear cache; make sure we get a new short code.
   shorten.clear()
   assert shorten.shorten(url)[0] == 'new'
